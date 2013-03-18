06580e59884003414114ccbde7166fd12ef20952
Arturs Kreipans <arturs.kreipans@gmail.com>
2013-03-18 16:17:49 +0000

    Missing file :)

--- lib/run_rabbit_run/processes/worker.rb
+++ lib/run_rabbit_run/processes/worker.rb
@@ -2,12 +2,6 @@ require 'run_rabbit_run/processes/base'
 
 module RunRabbitRun
   module Processes
-    class Worker < Base
-
-      def start &block
-        super &block
-      end
-
-    end
+    class Worker < Base; end
   end
 end
